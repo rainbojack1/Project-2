9716db3452aea82e017d8f9e3583b9da9a390e62
Jesselyn Jackson <rainbojack1@Users.NoReply.Github.com>
2019-09-19 18:29:58 -0400

    Changed the model names to singular and updated associations.

---
+++ models/buddy.js
@@ -0,0 +1,36 @@
+module.exports = function(sequelize, DataTypes) {
+  var Buddy = sequelize.define("Buddy", {
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    }
+  });
+
+  Buddy.associate = function(models) {
+    Buddy.belongsToMany(models.Interest, {
+      through: "BuddyInterests",
+      as: "interests",
+      foreignKey: "buddyId",
+      otherKey: "interestId"
+    });
+  };
+
+  return Buddy;
+};
