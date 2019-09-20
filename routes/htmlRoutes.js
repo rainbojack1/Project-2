e80e1f061bb05a329787600e3304e5c9f707cd9b
Seth Randell <sethhrandell@gmail.com>
2019-09-20 15:39:21 -0400

    added the logic for the buddy search but can't get it to see the array

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,4 +1,6 @@
 var db = require("../models");
+var Sequelize = require("sequelize");
+const Op = Sequelize.Op;
 
 module.exports = function(app) {
   // Load index page
@@ -55,21 +57,109 @@ module.exports = function(app) {
 
   // Load buddy page and pass in an buddy by id
   app.get("/buddy/:id", function(req, res) {
-    db.Buddy.findOne({ where: { id: req.params.id } }).then(function(dbBuddy) {
+    db.Buddy.findOne({
+      where: { id: req.params.id },
+      include: [
+        {
+          model: db.Interest,
+          as: "interests",
+          required: false,
+          // Pass in the Interest attributes that you want to retrieve
+          attributes: ["id", "name"],
+          through: {
+            // This block of code allows you to retrieve the properties of the join table
+            model: db.BuddyInterests,
+            as: "buddyInterests"
+          }
+        }
+      ]
+    }).then(function(dbBuddy) {
       res.render("buddy", {
         buddy: dbBuddy
       });
     });
   });
 
-  // app.get("/find", function(req, res) {
-  //   db.Buddy.findAll({}).then(function(dbBuddy) { Instead of dbbuddy put the name of the joined database
-  //     res.render("viewAll", {
-  //       msg: "Find a Buddy:",
-  //       //DATABASE HERE
-  //     });
-  //   });
-  // });
+  // Render find page for any finding buddies
+  app.get("/find", function(req, res) {
+    db.Interest.findAll({}).then(function(dbInterest) {
+      res.render("find", {
+        msg: "Find a Buddy:",
+        interests: dbInterest
+      });
+    });
+  });
+
+  var foundUserIds = [];
+  // Render find page for any finding buddies
+  app.get("/found", function(req, res) {
+    var arrayOfIds = [1, 5];
+    // var arrayOfUserIds = [99, 100];
+    db.BuddyInterest.findAll({
+      where: {
+        InterestId: {
+          [Op.in]: arrayOfIds
+        }
+      }
+    }).then(function(dbBuddyInterest) {
+      for (var i = 0; i < dbBuddyInterest.length; i++) {
+        console.log(dbBuddyInterest[i].BuddyId);
+        foundUserIds.push(dbBuddyInterest[i].BuddyId);
+      }
+    });
+    console.log(foundUserIds);
+    db.Buddy.findAll({
+      where: {
+        id: {
+          [Sequelize.Op.in]: foundUserIds
+        }
+      },
+      include: [
+        {
+          model: db.Interest,
+          as: "interests",
+          required: false,
+          // Pass in the Interest attributes that you want to retrieve
+          attributes: ["id", "name"],
+          through: {
+            // This block of code allows you to retrieve the properties of the join table
+            model: db.BuddyInterests,
+            as: "buddyInterests"
+          }
+        }
+      ]
+    }).then(function(dbBuddy) {
+      res.render("found", {
+        msg: "Found Buddies:",
+        buddies: dbBuddy
+      });
+    });
+  });
+
+  // db.Buddy.findAll({
+  //   include: [
+  //     {
+  //       model: db.Interest,
+  //       as: "interests",
+  //       required: false,
+  //       // Pass in the Interest attributes that you want to retrieve
+  //       attributes: ["id", "name"],
+  //       through: {
+  //         // This block of code allows you to retrieve the properties of the join table
+  //         model: db.BuddyInterests,
+  //         where: {
+  //           InterestId: {
+  //             [Op.in]: arrayOfIds
+  //           },
+  //           BuddyId: {
+  //             [Op.in]: arrayOfUserIds
+  //           }
+  //         },
+  //         as: "buddyInterests",
+  //         attributes: ["InterestId", "BuddyId"]
+  //       }
+  //     }
+  //   ]
 
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
